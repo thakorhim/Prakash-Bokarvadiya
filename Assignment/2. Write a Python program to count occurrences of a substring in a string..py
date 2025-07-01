28e19e6fd1cf9492005eaa3a998e0df897fb8f33
Prakash Bokarvadiya <117174109+thakorhim@users.noreply.github.com>
2025-07-01 11:10:30 +0530

    Delete Assignment directory

--- Assignment/2. Write a Python program to count occurrences of a substring in a string..py
+++
@@ -1,7 +0,0 @@
- # Python program to count occurrences of a substring in a string
-
-# Take input from the user
-main_string = input("Enter the main string: ")
-substring = input("Enter the substring to count: ")
-count = main_string.count(substring)                                      # Count occurrences using count() method
-print(f"The substring '{substring}' appears {count} times in the string.")   # Display the result
